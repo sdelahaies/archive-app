f5494425a0467654303fcfd9a4c21da493bf03e7
sdelahaies <sdelahaies@gmail.com>
2025-03-07 15:31:18 +0100

    fix: add logging

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -4,7 +4,10 @@ const SECRET_KEY = "your-secret-key";
 
 export async function POST(req) {
     const body = await req.json();
+    console.log(`${process.env.ADMIN_USERNAME}`)
+    console.log(`${process.env.ADMIN_PASSWORD}`)
     const { username, password } = body;
+    console.log(username, password)
     if (username === `${process.env.ADMIN_USERNAME}` && password === `${process.env.ADMIN_PASSWORD}`) {
         const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: "1h" });
         return new Response(JSON.stringify({ token }), { status: 200 });
